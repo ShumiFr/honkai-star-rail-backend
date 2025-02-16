9ccb9ac871eacface906b209e7af5aebd815d8ac
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2025-02-16 17:33:25 +0100

    changement de l'url

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: ["http://localhost:3000"], //"https://hsr-builder.netlify.app",
+  origin: "https://hsr-builder.netlify.app",
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
